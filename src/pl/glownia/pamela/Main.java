91ff66799bdd32ed3130e38ecc22077a570c9c65
Pam <pamela.glownia@gmail.com>
2021-07-23 18:58:49 +0200

    Verifying if the natural number is a Buzz number.

---
+++ src/pl/glownia/pamela/Main.java
@@ -0,0 +1,8 @@
+package pl.glownia.pamela;
+
+public class Main {
+    public static void main(String[] args) {
+        BuzzNumber buzzNumber = new BuzzNumber();
+        buzzNumber.checkUserNumber();
+    }
+}
