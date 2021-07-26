173ba7336be474e5eb9432e1a165ebd337e2865c
Pam <pamela.glownia@gmail.com>
2021-07-26 20:32:36 +0200

    Enabling checking few numbers in one time

--- src/pl/glownia/pamela/Number.java
+++ src/pl/glownia/pamela/Number.java
@@ -2,13 +2,13 @@ package pl.glownia.pamela;
 
 import java.util.Scanner;
 
-public class Number {
+public class Number implements Properties {
 
     boolean isNatural(long number) {
         if (number >= 0) {
             return true;
         } else {
-            System.out.println("The first parameter should be a natural number or zero.");
+            System.out.println("The parameters should be a natural number or zero.");
             return false;
         }
     }
@@ -59,28 +59,65 @@ public class Number {
         return false;
     }
 
+    @Override
+    public void printProperties(long number) {
+        if (isNatural(number)) {
+            System.out.println("Properties of " + number);
+            System.out.println("\t\teven:\t" + isEven(number));
+            System.out.println("\t\todd:\t" + isOdd(number));
+            System.out.println("\t\tbuzz:\t" + isBuzzNumber(number));
+            System.out.println("\t\tduck:\t" + isDuck(number));
+            System.out.println("palindromic:\t" + isPalindromic(number));
+            System.out.println("\tgapful:\t\t" + isGapful(number));
+        }
+    }
 
-    void printProperties(long number) {
-        System.out.println("Properties of " + number);
-        System.out.println("even:\t\t" + isEven(number));
-        System.out.println("odd:\t\t" + isOdd(number));
-        System.out.println("buzz:\t\t" + isBuzzNumber(number));
-        System.out.println("duck:\t\t" + isDuck(number));
-        System.out.println("palindromic: " + isPalindromic(number));
-        System.out.println("gapful:\t\t" + isGapful(number));
-
+    @Override
+    public void printProperties(long beginNumber, long counter) {
+        if (isNatural(beginNumber) && isNatural(counter)) {
+            for (long i = beginNumber; i < beginNumber + counter; i++) {
+                System.out.print(i + " is ");
+                if (isEven(i)) {
+                    System.out.print("even");
+                }
+                if (isOdd(i)) {
+                    System.out.print("odd");
+                }
+                if (isBuzzNumber(i)) {
+                    System.out.print(", buzz ");
+                }
+                if (isDuck(i)) {
+                    System.out.print(", duck");
+                }
+                if (isPalindromic(i)) {
+                    System.out.print(", palindromic");
+                }
+                if (isGapful(i)) {
+                    System.out.print(", gapful");
+                }
+                System.out.println();
+            }
+        }
     }
 
     void checkUserNumber() {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter a request: ");
-        long number = scan.nextLong();
-        while (number != 0L) {
-            if (isNatural(number)) {
+        String userNumber = scan.nextLine();
+        while (!(userNumber.equals("0"))) {
+            if (userNumber.equals("")) {
+                printWelcome();
+            } else if (userNumber.contains(" ")) {
+                String[] array = userNumber.split(" ");
+                long beginNumber = Long.parseLong(array[0]);
+                long counter = Long.parseLong(array[1]);
+                printProperties(beginNumber, counter);
+            } else {
+                long number = Long.parseLong(userNumber);
                 printProperties(number);
             }
             System.out.print("Enter a request: ");
-            number = scan.nextLong();
+            userNumber = scan.nextLine();
         }
         System.out.println("Goodbye!");
     }
@@ -88,7 +125,11 @@ public class Number {
     void printWelcome() {
         System.out.println("Welcome to Amazing Numbers!");
         System.out.println("Supported requests:");
-        System.out.println("- enter a natural number to know its properties,");
+        System.out.println("- enter a natural number to know its properties;");
+        System.out.println("- enter two natural numbers to obtain the properties of the list:");
+        System.out.println("\t *the first parameter represents a starting number;");
+        System.out.println("\t *the second parameter shows how many consecutive numbers are to be printed;");
+        System.out.println("- separate the parameters with one space;");
         System.out.println("- enter 0 to exit.");
     }
 
---
+++ src/pl/glownia/pamela/Properties.java
@@ -0,0 +1,8 @@
+package pl.glownia.pamela;
+
+public interface Properties {
+
+    void printProperties(long number);
+
+    void printProperties(long beginNumber, long finishNumber);
+}
